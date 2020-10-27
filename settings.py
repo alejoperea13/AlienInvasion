62176d3c6af5cc7ee30623dff6c5f8e4dcc7cd3e
Alejandro <alejoperea13@hotmail.com>
2020-10-27 10:19:27 -0400

    aliens fleet and new ship

--- settings.py
+++ settings.py
@@ -1,12 +1,16 @@
+import pygame
+
+
 class Settings:
     """A class to store all settings for Alien Invasion"""
 
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings.
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 540
+        self.screen_height = 350
         self.bg_color = (230, 230, 230)
+        self.bg_image = pygame.image.load('images/bg.png')
 
         # Ship settings.
         self.ship_speed = 1.5
@@ -15,5 +19,5 @@ class Settings:
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
