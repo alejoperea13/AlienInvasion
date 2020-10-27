1ed53633cf5c77046658d36de0c17392d9d64dd6
Alejandro <alejoperea13@hotmail.com>
2020-10-27 14:16:05 -0400

    Fleet moving and dropping down

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings.
         self.screen_width = 720
-        self.screen_height = 400
+        self.screen_height = 500
         self.bg_color = (230, 230, 230)
         self.bg_image = pygame.image.load('images/bg.png')
 
@@ -21,3 +21,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (255, 0, 0)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
